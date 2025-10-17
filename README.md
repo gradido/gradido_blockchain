de22cf920addc441366426fdce1b4699738ef9f0
einhornimmond <info@einhornimmond.de>
2025-10-17 13:43:21 +0200

    cross compile for arm64 on linux x64

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # Gradido Blockchain
+
 lib for all gradido blockchain related stuff in c++
 
 git submodule update --init --recursive
 
 ## Dependencies
-- cmake 
-- rust
+
+- cmake
+- zig
+
+## Install in Debian 12
+
+```bash
+sudo apt install libsodium-dev libssl-dev
+```
 
 ## Build with CMake in Release Mode
+
 Without parameter is version without test, http and iota rust client
 
 ```bash
@@ -19,13 +28,15 @@ cmake --build .
 ```
 
 ## test and https on windows
+
 ```bash
 cd gradido_blockchain
 mkdir build
 cd build
 cmake --preset windows-x64 -DENABLE_TEST=ON -DENABLE_HTTPS=ON ..
 ctest
-``` 
+```
+
 on windows, vcpkg (submodule) is used to download and build openssl for https support
 
 - ENABLE_IOTA_RUST_CLIENT: switch for include rust iota client into build, needed for sending transaction to iota
@@ -34,14 +45,25 @@ on windows, vcpkg (submodule) is used to download and build openssl for https su
 - ENABLE_HTTPS: switch for include openssl for https support
 
 ## Install for Linux
+
 ```bash
 mkdir build
 cd build
 cmake ..
-# build protoc 
+# build protoc
 make -j$(nproc) protoc
 # parse protoc to c++
 cmake ..
 # build lib
+make -j$(nproc) GradidoBlockchain
+```
+
+## Cross Compile for arm64 on Linux
+
+Then in project root
+
+```bash
+mkdir build_arm64 && cd build_arm64
+cmake .. -DTARGET=aarch64-linux-gnu
 make -j$(nproc) GradidoBlockchain
 ```
--- toolchain.cmake
+++ toolchain.cmake
@@ -2,3 +2,39 @@ set(CMAKE_C_COMPILER zig cc)
 set(CMAKE_CXX_COMPILER zig c++)
 set(CMAKE_C_COMPILER_TARGET ${TARGET})
 set(CMAKE_CXX_COMPILER_TARGET ${TARGET})
+
+#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+
+
+
+if( "${TARGET}" STREQUAL "aarch64-linux-gnu" AND NOT CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64")
+  # CROSS COMPILING FOR ARM64 LINUX ON NON-ARM64 HOST
+  message(STATUS "Cross compiling for arm64-linux-gnu")
+  set(CMAKE_SYSTEM_NAME Linux)          # Target OS
+  set(CMAKE_SYSTEM_PROCESSOR aarch64)  # Target CPU
+  find_program(QEMU_AARCH64_EXECUTABLE qemu-aarch64)
+  if(QEMU_AARCH64_EXECUTABLE)
+    set(CMAKE_CROSSCOMPILING_EMULATOR ${QEMU_AARCH64_EXECUTABLE})
+  else()
+    message(WARNING "qemu-aarch64 not found, test don't run automatically")
+  endif()
+endif()
+
+# set correct vcpkg triplet
+if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    if (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+        set(VCPKG_TARGET_TRIPLET "arm64-linux")
+    else()
+        set(VCPKG_TARGET_TRIPLET "x64-linux")
+    endif()
+elseif (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(VCPKG_TARGET_TRIPLET "x64-windows")
+    else()
+        set(VCPKG_TARGET_TRIPLET "x86-windows")
+    endif()
+endif()
