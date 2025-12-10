32fb19e801ea5ccfef3836c6ed940829b9ff8661
einhornimmond <silas@einhornimmond.de>
2025-12-10 08:24:42 +0100

    reducing unneccessary data copy

--- src/interaction/deserialize/GradidoTransactionRole.cpp
+++ src/interaction/deserialize/GradidoTransactionRole.cpp
@@ -16,23 +16,24 @@ namespace gradido {
 				memory::BlockPtr bodyBytesPtr;
 				memory::BlockPtr paringMessageIdPtr = nullptr;
 				
-				auto sigMap = message["sig_map"_f];
-				if (sigMap.has_value() && sigMap.value()["sig_pair"_f].size()) {
-					auto sigPairs = sigMap.value()["sig_pair"_f];
-					for (int i = 0; i < sigPairs.size(); i++) {
+				const auto& sigMap = &message["sig_map"_f];
+				if (sigMap->has_value() && sigMap->value()["sig_pair"_f].size()) {
+					const auto& sigPairs = &sigMap->value()["sig_pair"_f];
+					signatures.reserve(sigPairs->size());
+					for (int i = 0; i < sigPairs->size(); i++) {
 						signatures.push(data::SignaturePair(
-							std::make_shared<memory::Block>(sigPairs[i]["pubkey"_f].value()),
-							std::make_shared<memory::Block>(sigPairs[i]["signature"_f].value())
+							std::make_shared<memory::Block>((*sigPairs)[i]["pubkey"_f].value()),
+							std::make_shared<memory::Block>((*sigPairs)[i]["signature"_f].value())
 						));
 					}
 				}
-				auto bodyBytes = message["body_bytes"_f];
-				if (bodyBytes.has_value()) {
-					bodyBytesPtr = std::make_shared<memory::Block>(bodyBytes.value());
+				const auto& bodyBytes = &message["body_bytes"_f];
+				if (bodyBytes->has_value()) {
+					bodyBytesPtr = std::make_shared<memory::Block>(bodyBytes->value());
 				}
-				auto paringMessageId = message["parent_message_id"_f];
-				if (paringMessageId.has_value()) {
-					paringMessageIdPtr = std::make_shared<memory::Block>(paringMessageId.value());
+				const auto& paringMessageId = &message["parent_message_id"_f];
+				if (paringMessageId->has_value()) {
+					paringMessageIdPtr = std::make_shared<memory::Block>(paringMessageId->value());
 				}
 				mGradidoTransaction = std::make_unique<const data::GradidoTransaction>(signatures, bodyBytesPtr, paringMessageIdPtr);
 			}
