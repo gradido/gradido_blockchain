56efb07722b9aba887e8c295e746b9a97c76dede
einhornimmond <silas@einhornimmond.de>
2026-03-04 12:25:27 +0100

    add std::stop_source

--- include/gradido_blockchain/Application.h
+++ include/gradido_blockchain/Application.h
@@ -5,6 +5,7 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <stop_token>
 
 /*
 * Class with loop for longer running Apps
@@ -18,16 +19,17 @@ public:
 	//! call init and start main loop if init return true
 	virtual void run();
 
-	static void terminate() { gRunning = false; mExitCondition.notify_one(); }
+	static void terminate() { mMasterStopSource.request_stop(); gRunning = false; mExitCondition.notify_one(); }
 	static std::atomic<bool> gRunning;
+	std::stop_token getStopToken() const { return mMasterStopSource.get_token(); }
 protected:
 	//! \return true on success, false on failure, if return false, exit will be called and program will end
 	virtual bool init() = 0;
 	virtual void exit() = 0;
-
-	
+		
 	static std::mutex mConditionMutex;
 	static std::condition_variable mExitCondition;
+	static std::stop_source mMasterStopSource;	
 };
 
 #endif //__GRADIDO_BLOCKCHAIN_SERVER_APPLICATION_H
